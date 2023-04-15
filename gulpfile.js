f54bd50bbee36d2d862501a91e66119312d038bb
skyhanullee <48109897+skyhanullee@users.noreply.github.com>
2023-04-15 04:26:20 -0700

    reorganized file structure

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-  return src('index.scss')
+  return src('bootstrap-clone/**/*.scss')
     .pipe(sass())
     .pipe(dest('css')); // output into css file in directory
 }
 
 function watchTask() {
-  watch(['index.scss'], buildStyles);
+  watch(['bootstrap-clone/**/*.scss'], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
